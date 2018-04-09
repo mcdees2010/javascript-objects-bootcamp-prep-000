f69d7273b7f1a10d0744bb7f7bba480b04a08b85
Maria Dees <kingsgrl2010@yahoo.com>
2018-04-09 00:02:33 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,6 +7,7 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
 }
  
 function destructivelyUpdateObjectWithKeyAndValue(playlist, artistName, songTitle) {
+  updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
   
 }
 
