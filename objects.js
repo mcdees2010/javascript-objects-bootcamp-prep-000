e468b286bfc9088351091ae69b8481e59abbea64
Maria Dees <kingsgrl2010@yahoo.com>
2018-04-09 00:27:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,9 @@
 var playlist = {madonna: "Vogue"}
  
 function updatePlaylist(playlist, artistName, songTitle) {
-  
+ playlist [] = songTitle;
+    return playlist;
+}
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.Slowdive;
