6a80661df657eceabe119d8cfeca6f4990036f29
Maria Dees <kingsgrl2010@yahoo.com>
2018-04-09 00:19:16 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {madonna: "Vogue"}
  
 function updatePlaylist(playlist, artistName, songTitle) {
   Object.assign({}, playlist, {'Slowdive': 'Alison'})
-  Object.assign({}, playlist, {})
+  Object.assign({}, playlist, {'My Bloody Valentine': 'Sometimes'})
 }
 
 function removeFromPlaylist(playlist, artistName) {
