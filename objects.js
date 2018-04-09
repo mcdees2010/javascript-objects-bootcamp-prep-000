d0b02756304415ea271d416597f6aa3672cf4de0
Maria Dees <kingsgrl2010@yahoo.com>
2018-04-09 00:04:11 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,14 +1,2 @@
 var playlist = {madonna: "Vogue", beyonce: "Halo", pink: "Try"}
  
-function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
-  obj[key] = value
- 
-  return obj
-}
- 
-function destructivelyUpdateObjectWithKeyAndValue(playlist, artistName, songTitle) {
-  updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
-  
-}
-
-playlist
