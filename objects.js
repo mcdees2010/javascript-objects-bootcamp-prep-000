09a04101909309078bb7c378789e1d0bc6414a60
Maria Dees <kingsgrl2010@yahoo.com>
2018-04-09 00:23:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 
 
-var playlist = {aristName: songTitle}
+var playlist = {artistName: songTitle}
  
 function updatePlaylist(playlist, artistName, songTitle) {
   Object.assign({}, playlist, {'Slowdive': 'Alison'})
